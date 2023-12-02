27207f0510fd9601cf30caf598a758030f93b1e4
krystiano13 <crystianotv@gmail.com>
2023-12-02 19:49:36 +0100

    create desktop version for info section

---
+++ src/utils/infoData.ts
@@ -0,0 +1,30 @@
+import egg from '../assets/images/desktop/image-transform.jpg';
+import pink from '../assets/images/desktop/image-stand-out.jpg';
+
+type info = {
+    title: string;
+    content: string;
+    img: ImageMetadata;
+    alt: string;
+    color: string;
+    flexDirection: string;
+}
+
+export const infoData:info[] = [
+    {
+        title: "Transform your brand",
+        content: "We are a full-service creative agency specializing in helping brands grow fast.Engage your clients through compelling visuals that do most of the marketing for you",
+        color: "hsl(51, 100%, 49%)",
+        flexDirection: "row",
+        img: egg,
+        alt: "egg image"
+    },
+    {
+        title: "Stand out to the right audience",
+        content: "Using a collaborative formula of designers, researchers, photographers, videographers and copywriters, we'll build and extend your brand in digital places",
+        color: "hsl(7, 99%, 70%)",
+        flexDirection: "row-reverse",
+        img: pink,
+        alt: "pink image"
+    },
+]
