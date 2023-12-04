a0dff49558058e42d47e162b9e0dfad47329bbed
krystiano13 <crystianotv@gmail.com>
2023-12-04 13:56:53 +0100

    create script for toggling hamburger menu

---
+++ src/utils/hamburger.ts
@@ -0,0 +1,17 @@
+const hamburgerMenu:HTMLDivElement = document.querySelector('.hamburgerMenu');
+const hamburgerButton:HTMLDivElement = document.querySelector('.hamburger');
+
+let shown:boolean = false;
+hamburgerMenu.style.display = 'none';
+
+hamburgerButton.addEventListener('click', ():void => {
+    shown = !shown;
+
+    if(shown) {
+        hamburgerMenu.style.display = 'flex';
+    }
+
+    else {
+        hamburgerMenu.style.display = 'none';
+    }
+})
